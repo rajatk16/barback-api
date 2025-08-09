1cd98c0405f3fe827123a82c5b7d07bf0bc4c438
geekrajat <rajat.sudagade@gmail.com>
2025-08-09 16:33:48 +0530

    Add authentication module with JWT support, user registration and login functionality (#1)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { RecipesModule } from './recipes/recipes.module';
+import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 @Module({
-  imports: [AuthModule, RecipesModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production').required(),
+        PORT: Joi.number().default(3000),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().default('1h'),
+        JWT_REFRESH_SECRET: Joi.string().required(),
+        JWT_REFRESH_EXPIRES_IN: Joi.string().default('7d'),
+        DATABASE_URL: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+    RecipesModule,
+    UsersModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
 
-@Module({})
+import { AuthService } from './auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  exports: [AuthService],
+  providers: [AuthService],
+  controllers: [AuthController],
+  imports: [
+    PrismaModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.getOrThrow<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.getOrThrow<string>('JWT_EXPIRES_IN'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
 export class AuthModule {}
